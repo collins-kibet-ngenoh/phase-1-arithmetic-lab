f89be1b03d88770395f5082e3b947f5296be19ca
collins-kibet-ngenoh <collins.kibet.ngenoh@gmail.com>
2024-03-21 19:30:01 +0300

    does arithmnetics

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+let num1 =2;
+let num2 =31;
+let multiply=num1*num2;
+let random= Math.floor(Math.random()*10)+1;
+let num3=13;
+let num4=17;
+let mod=num4 % num3;
+let max= Math.max(5,20);
